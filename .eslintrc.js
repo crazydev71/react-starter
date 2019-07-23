31648bfbdd9e1bc327fa953bd21d37e4d7c3e3f5
crazydev71 <crazy.dev@aol.com>
2019-07-23 10:21:35 +1000

    Update dependences

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
+    "jasmine": true,
   },
   "extends": ["airbnb", "plugin:prettier/recommended"],
   "plugins": ["jsx-a11y",  "prettier"],
   "rules": {
     "react/jsx-filename-extension": [1, {"extensions": [".js"]}],
+    "react/jsx-one-expression-per-line": 0,
   },
 };
